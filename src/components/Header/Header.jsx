28cf9ad6992f5627740c53ea86186647b1c80685
mmmrnm33 <mmmrnm33@gmail.com>
2024-09-25 23:39:29 +0900

    feat: 헤더 컴포넌트에 프로필 드랍다운 기능 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { auth } from "../../firebase";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { CgProfile } from "react-icons/cg";
 
 const Header = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [nickname, setNickname] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const db = getFirestore();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setNickname(userData.nickname);
+        }
+      } else {
+        setUser(null);
+        setNickname("");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [db]);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+    setShowDropdown(false);
+  };
+
   return (
     <>
       <header className={styles.header}>
@@ -15,20 +53,31 @@ const Header = ({ children }) => {
                 거래소
               </Link>
             </li>
-            <li className={styles.navItem}>
-              <Link to="/" className={styles.navLink}>
-                자산
-              </Link>
-            </li>
           </ul>
         </nav>
         <div className={styles.authButtons}>
-          <Link to="/" className={styles.loginButton}>
-            로그인
-          </Link>
-          <Link to="/signup" className={styles.signupButton}>
-            회원가입
-          </Link>
+          {user ? (
+            <div className={styles.userInfo} onClick={toggleDropdown}>
+              <CgProfile className={styles.profileIcon} />
+              {showDropdown && (
+                <div className={styles.dropdownMenu}>
+                  <p className={styles.dropdownItem}>{nickname}</p>
+                  <p className={styles.dropdownItem} onClick={handleLogout}>
+                    로그아웃
+                  </p>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              <Link to="/login" className={styles.loginButton}>
+                로그인
+              </Link>
+              <Link to="/signup" className={styles.signupButton}>
+                회원가입
+              </Link>
+            </>
+          )}
         </div>
       </header>
       <main>{children}</main>
